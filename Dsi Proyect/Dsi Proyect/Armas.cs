a0339d4d5e67b01ecc701353f4f35179459325a1
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2023-04-10 20:38:05 +0200

    Menus de tienda y seleccion de equipo hechos

---
+++ Dsi Proyect/Dsi Proyect/Armas.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace Dsi_Proyect
+{
+    public class Armas
+    {
+        public string Id;
+        public Image Imagen;
+        public string Nombre;
+        public int X;
+        public int Y;
+        public Armas() { }
+    }
+    public class ListaArmas
+    {
+        public static List<Armas> weapons = new List<Armas>()
+        {
+            new Armas()
+            {
+                Id = "Sword 1",
+                Nombre = "Espada de madera",
+                X = 150,
+                Y = 150
+            },
+            new Armas()
+            {
+                Id = "Sword 2",
+                Nombre = "Espada de piedra",
+                X = 100,
+                Y = 100
+            },
+            new Armas()
+            {
+                Id = "Sword 3",
+                Nombre = "Espada de acero",
+                X = 50,
+                Y = 50
+            },
+            new Armas()
+            {
+                Id = "Sword 4",
+                Nombre = "Espada de oro",
+                X = 200,
+                Y = 200
+            },
+            new Armas()
+            {
+                Id = "Sword 5",
+                Nombre = "Espada de diamante",
+                X = 250,
+                Y = 250
+            }
+        };
+    }
+}
