9126305e93ecc337c928034e0da0218220b12dbf
LittleWallSt <javmuril@ucm.es>
2023-04-19 11:51:58 +0200

    cambios para empezar

--- Dsi Proyect/Dsi Proyect/MenuPrincipal.xaml.cs
+++ Dsi Proyect/Dsi Proyect/MenuPrincipal.xaml.cs
@@ -26,5 +26,10 @@ namespace Dsi_Proyect
         {
             this.InitializeComponent();
         }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(TeamSelect));
+        }
     }
 }
